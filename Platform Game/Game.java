66b5c0f07330714b7a940fef2b94656317298185
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-05-12 10:52:50 -0600

    Add skeleton code

---
+++ Platform Game/Game.java
@@ -0,0 +1,13 @@
+package com.realtutsgml.neon.window;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable
+{
+    private static final long serialVersionUID = -6261436164361361187L;
+
+    public void run()
+    {
+
+    }
+}
