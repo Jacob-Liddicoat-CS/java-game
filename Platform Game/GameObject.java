9c2dc4e8e7a4af0bca70f4d409eabd96736dbfce
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-05-12 12:49:41 -0600

    Add basic functions for GameObject

--- Platform Game/GameObject.java
+++ Platform Game/GameObject.java
@@ -1,8 +1,31 @@
 package com.realtutsgml.neon.framework;
 
+import java.awt.Graphics;
+import java.util.LinkedList;
+
 public abstract class GameObject
 {
 
-    
+    protected float x, y;
+    protected float velX = 0, velY = 0;
+
+    public GameObject(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract void tick(LinkedList<GameObject> object);
+    public abstract void render(Graphics g);
+
+    public abstract float getX();
+    public abstract float getY();
+    public abstract void setX(float x);
+    public abstract void setY(float y);
+
+    public abstract float getVelX();
+    public abstract float getVelY();
+    public abstract void setVelX(float VelX);
+    public abstract void setVelY(float velY);
 
 }
