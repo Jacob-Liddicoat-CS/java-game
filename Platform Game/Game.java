f559bb8d4726f339c4da895671797b11f01b7d37
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-05-12 11:28:46 -0600

    Add thread, synchronized method and failsafe

--- Platform Game/Game.java
+++ Platform Game/Game.java
@@ -6,9 +6,21 @@ public class Game extends Canvas implements Runnable
 {
     private static final long serialVersionUID = -6261436164361361187L;
 
+    private boolean running = false;
+    private Thread thread;
+
+    public synchronized void start(){
+        if(running)
+            return;
+
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
     public void run()
     {
-
+        System.out.println("Thread has begun");
     }
 
     public static void main(String args[]){
