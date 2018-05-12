ee0999469cb55ed70e86ead0d1ca8ec1c42075af
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-05-12 11:12:46 -0600

    Add Window Properties

--- Platform Game/Window.java
+++ Platform Game/Window.java
@@ -2,8 +2,19 @@ package com.realtutsgml.neon.window;
 
 public class Window {
 
-  public Window(int w, int h, String title)
+  public Window(int w, int h, String title, Game game)
   {
+    game.setPreferredSize(new Dimension(w, h));
+    game.setMaximumSize(new Dimension(w, h));
+    game.setMinimumSize(new Dimension(w, h));
+
+    JFrame frame = new JFrame(title);
+    frame.add(game);
+    frame.pack();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setResizable(false);
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
     
   }
 }
