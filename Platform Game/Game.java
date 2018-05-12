bff3a2d40d8b9a24be0b36b278cb731687776c18
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-05-12 12:15:05 -0600

    Add FrameRate monitor

--- Platform Game/Game.java
+++ Platform Game/Game.java
@@ -20,7 +20,42 @@ public class Game extends Canvas implements Runnable
 
     public void run()
     {
-        System.out.println("Thread has begun");
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int updates = 0;
+        int frames = 0;
+        while(running){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            while(delta >= 1){
+                tick();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println("FPS: " + frames + "TICKS: " + updates);
+                frames = 0;
+                updates = 0;
+            }
+        }
+    }
+
+    private void tick()
+    {
+
+    }
+
+    private void render()
+    {
+        
     }
 
     public static void main(String args[]){
